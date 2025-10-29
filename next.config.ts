32cbf901cc49f53632a9d6e2dde7152d7cf5abb1
Mark Procopio <mprocopio@evertz.com>
2025-10-29 16:09:04 -0400

    more testing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //   typescript: {
+  //   // Allow production build even when TypeScript reports errors
+  //   ignoreBuildErrors: true,
+  // },
+  eslint: {
+    // Optional: also skip ESLint during build
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
